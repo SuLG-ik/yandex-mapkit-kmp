3b3dba7bbc982bbbed5bb998dc4e992cb6e72e0a
SuLG-k <nenashkinvov@gmail.com>
2024-08-29 00:28:27 +0700

    change library publication strategy from branch

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,5 @@
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.multiplatform).apply(false)
     alias(libs.plugins.kotlin.android).apply(false)
@@ -12,5 +14,47 @@ plugins {
 }
 
 subprojects {
+    extra.set("library_version", getProperty("library_version", "null"))
+
     group = "ru.sulgik.mapkit"
+}
+
+fun Project.getProperty(name: String, defaultValue: String): String {
+    return getProperty(name) ?: defaultValue
+}
+
+fun Project.getProperty(name: String): String? {
+    return getEnvironmentProperty(name)
+        ?: rootProject.getLocalProperty(name)
+        ?: rootProject.findProperty(name)?.toString()
+}
+
+fun getEnvironmentProperty(name: String, defaultValue: String): String? {
+    return getEnvironmentProperty(name) ?: defaultValue
+}
+
+fun getEnvironmentProperty(name: String): String? {
+    return System.getenv()[name]
+}
+
+fun Project.getLocalProperty(name: String, defaultValue: String): String {
+    return getLocalProperty(name) ?: defaultValue
+}
+
+fun Project.getLocalProperty(name: String): String? {
+    return getLocalProperties().getProperty(name)
+}
+
+fun Project.getLocalProperties(): Properties {
+    return loadLocalProperties()
+}
+
+fun Project.loadLocalProperties(fileName: String = "local.properties"): Properties {
+    return Properties().also { properties ->
+        try {
+            file(fileName).inputStream().use { properties.load(it) }
+        } catch (e: Exception) {
+            logger.info("$fileName not found, skip loading properties")
+        }
+    }
 }
