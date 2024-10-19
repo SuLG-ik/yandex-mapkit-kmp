984d65cbc7a66def17f126910491add7e4bae887
SuLG-k <nenashkinvov@gmail.com>
2024-10-19 21:25:43 +0700

    fix mapview ios gestures interop

--- yandex-mapkit-kmp-compose/src/iosMain/kotlin/ru/sulgik/mapkit/compose/NativeYandexMap.ios.kt
+++ yandex-mapkit-kmp-compose/src/iosMain/kotlin/ru/sulgik/mapkit/compose/NativeYandexMap.ios.kt
@@ -2,12 +2,16 @@ package ru.sulgik.mapkit.compose
 
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.viewinterop.UIKitInteropInteractionMode
+import androidx.compose.ui.viewinterop.UIKitInteropProperties
 import androidx.compose.ui.viewinterop.UIKitView
 import ru.sulgik.mapkit.mapview.MapView
 import ru.sulgik.mapkit.mapview.toCommon
 import YandexMapKit.YMKMapView as NativeMapView
 
+@OptIn(ExperimentalComposeUiApi::class)
 @Composable
 internal actual fun NativeYandexMap(
     modifier: Modifier,
@@ -27,5 +31,8 @@ internal actual fun NativeYandexMap(
             update(mapView)
         },
         modifier = modifier,
+        properties = UIKitInteropProperties(
+            interactionMode = UIKitInteropInteractionMode.NonCooperative,
+        )
     )
 }
