74e65aa70d054a4732c5a02c995c85611f015a8c
altynbek <n.altynbek.kz@gmail.com>
2025-02-20 13:51:28 +0500

    add Geo

---
+++ yandex-mapkit-kmp/src/commonMain/kotlin/ru/sulgik/mapkit/geometry/Geo.kt
@@ -0,0 +1,18 @@
+package ru.sulgik.mapkit.geometry
+
+public expect object Geo {
+
+    public fun distance(firstPoint: Point, secondPoint: Point): Double
+
+    public fun closestPoint(point: Point, segment: Segment): Point
+
+    public fun pointOnSegmentByFactor(segment: Segment, factor: Double): Point
+
+    public fun course(firstPoint: Point, secondPoint: Point): Double
+
+    public fun advancePolylinePosition(
+        polyline: Polyline,
+        position: PolylinePosition,
+        distance: Double,
+    ): PolylinePosition
+}
