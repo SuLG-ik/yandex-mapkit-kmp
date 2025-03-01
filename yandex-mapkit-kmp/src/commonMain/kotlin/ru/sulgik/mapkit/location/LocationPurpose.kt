9f94f8b37aa371b7db87f09dea30dec739e72f40
SuLGik <58724087+SuLG-ik@users.noreply.github.com>
2025-03-01 13:13:38 +0700

    Version update breaking changes. Refactor location updates subscription and add indoor level ID
    *   Remove `FilteringMode` and replace it with `UseInBackground` in `SubscriptionSettings`.
    *   Add `SubscriptionSettings` class to encapsulate location subscription parameters.
    *   Update `LocationManager` to use `SubscriptionSettings` for subscribing to location updates.
    *   Update location purpose to add `AUTOMOTIVE_NAVIGATION`, `PEDESTRIAN_NAVIGATION`, `BICYCLE_NAVIGATION`, and `SCOOTER_NAVIGATION`.
    *   Add `indoorLevelId` to the `Location` class.
    *   Add `UseInBackground` enum.

--- yandex-mapkit-kmp/src/commonMain/kotlin/ru/sulgik/mapkit/location/LocationPurpose.kt
+++ yandex-mapkit-kmp/src/commonMain/kotlin/ru/sulgik/mapkit/location/LocationPurpose.kt
@@ -2,5 +2,9 @@ package ru.sulgik.mapkit.location
 
 public enum class LocationPurpose {
     GENERAL,
-    NAVIGATION
+    AUTOMOTIVE_NAVIGATION,
+    PEDESTRIAN_NAVIGATION,
+    BICYCLE_NAVIGATION,
+    SCOOTER_NAVIGATION,
+    ;
 }
